1fa5c1fbefc735d1480a474f559f5746d93ef365
Ray <ray.greenan.89@outlook.com>
2018-10-19 15:47:12 +0100

    petshop_weekend_HW

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -13,3 +13,21 @@ end
 def add_or_remove_cash(pet_shop, value)
   return pet_shop[:admin][:total_cash] += value
 end
+
+
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+
+
+def increase_pets_sold(pet_shop, sold_pets)
+  return pet_shop[:admin][:pets_sold] += sold_pets
+end
+
+
+
+def stock_count(pet_shop)
+  return pet_shop += [:pets]
+end
